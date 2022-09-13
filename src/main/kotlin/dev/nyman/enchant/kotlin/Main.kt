8bb858f0aa51cad089dcba65566a9d0936a76630
Markus Nyman <nymak@tuta.io>
2022-09-13 04:07:18 +0300

    implement dict_check and dict_suggest

--- src/main/kotlin/dev/nyman/enchant/kotlin/Main.kt
+++ src/main/kotlin/dev/nyman/enchant/kotlin/Main.kt
@@ -1,12 +1,20 @@
 package dev.nyman.enchant.kotlin
 
 import com.sun.jna.Native
+import com.sun.jna.ptr.IntByReference
 import dev.nyman.enchant.kotlin.enchant.EnchantLib
 
 fun main() {
     val enchant = Native.load("enchant-2", EnchantLib::class.java) as EnchantLib
     val broker = enchant.enchant_broker_init()
     val dict = enchant.enchant_broker_request_dict(broker, "fi")
+    println(enchant.enchant_dict_check(dict, "kissa", 5))
+    println(enchant.enchant_dict_check(dict, "kisssa", 6))
+
+    val ns = IntByReference()
+    val suggestions = enchant.enchant_dict_suggest(dict, "kisssa", 6, ns)
+    suggestions.forEach(::println)
+
     enchant.enchant_broker_free_dict(broker, dict)
     enchant.enchant_broker_free(broker)
 }
