a4650de87d77a585525b561695d00c0a68a9f348
Markus Nyman <nymak@tuta.io>
2022-09-13 05:30:04 +0300

    remove Main and tests

--- src/main/kotlin/dev/nyman/enchant/kotlin/Enchant.kt
+++ src/main/kotlin/dev/nyman/enchant/kotlin/Enchant.kt
@@ -4,22 +4,26 @@ import com.sun.jna.Native
 import dev.nyman.enchant.kotlin.jna.EnchantBrokerStruct
 import dev.nyman.enchant.kotlin.jna.EnchantDictStruct
 import dev.nyman.enchant.kotlin.jna.EnchantLib
-import java.lang.Exception
 
 const val LIB_NAME = "enchant-2"
 
 class Enchant {
-    private val lib: EnchantLib by lazy { Native.load(LIB_NAME, EnchantLib::class.java) }
-    private val broker: EnchantBrokerStruct by lazy { lib.enchant_broker_init() }
+    private val lib: EnchantLib = Native.load(LIB_NAME, EnchantLib::class.java)
+    private val broker: EnchantBrokerStruct = lib.enchant_broker_init()
 
     fun getVersion(): String = lib.enchant_get_version()
 
     fun dictExists(languageTag: String) = lib.enchant_broker_dict_exists(broker, languageTag) > 0
 
-    fun loadDict(languageTag: String): EnchantDict {
+    fun loadDict(languageTag: String): EnchantDict? {
         val dict = lib.enchant_broker_request_dict(broker, languageTag)
-        return dict?.let { EnchantDict(lib, this::freeDict, it) }
-            ?: throw Exception("Dictionary with languageTag: $languageTag not found")
+        val enchantDict = dict?.let { EnchantDict(lib, this::freeDict, it) }
+
+        if (enchantDict == null) {
+            println("Failed to load dictionary with languageTag: $languageTag")
+        }
+
+        return enchantDict
     }
 
     private fun freeDict(dict: EnchantDictStruct) {
